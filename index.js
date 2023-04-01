fbb05139858c31816fb69f103b08cdef733222c6
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-04-01 19:57:18 +0300

    added new class toDoStyle to work in CSS

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const inputArea = document.querySelector('inputArea');
-const btn = document.querySelector('btn');
-const toDoList = document.querySelector('toDoList');
+const inputArea = document.querySelector('.inputArea');
+const btn = document.querySelector('.btn');
+const toDoList = document.querySelector('.toDoList');
 
 btn.addEventListener('click', function() {
 const item = document.createElement('li');
 item.innerText = inputArea.value;
+item.classList.add('toDoStyle');
 toDoList.appendChild(item);
+inputArea.value = '';
+
+
 })
